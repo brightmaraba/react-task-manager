f367f5ee35012eeae16a73c143b32812ec70fa5a
Brian Koech <brightkoech@gmail.com>
2021-03-17 03:22:39 +0800

    Noticed unstaged changes.

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,8 +4,11 @@ const Task = ( {task, onDelete}) => {
     return (
         <div className='task'>
         <h3>
-            {task.text} {' '} <FaTimes style={{ color: 'red', cursor: 'pointer' }} 
-            onClick={() => onDelete(task.id)} />
+            {task.text} {' '}
+            <FaTimes
+            style={{ color: 'red', cursor: 'pointer' }}
+            onClick={() => onDelete(task.id)}
+            />
         </h3>
         <p>{task.day}</p>
         </div>
