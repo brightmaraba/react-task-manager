b2b1864e71d6c43eecc3926270dc0f22f67b26c7
Brian Koech <brightkoech@gmail.com>
2021-03-24 08:58:35 +0800

    Completed Json Server, Routing

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,8 @@ import { Link } from 'react-router-dom'
 function About() {
     return (
         <div>
-            <h4>Version: 1.0.0</h4>
+            <h2>Version: 1.0.0</h2>
+            <h4>Released: Mar 24, 2021</h4>
             <Link to='/'>Go back</Link>
         </div>
     )
